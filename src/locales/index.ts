835f821feda8947e23fb562398581fafe3247aca
Gabriel Guilhoto <gabriel.guilhoto@geekie.com.br>
2021-11-04 18:30:47 -0300

    Adds pt-BR translation
    This commit adds support for a Brazilian Portuguese locale, by creating 
    a locale file on src/locales.  I included translations for the 
    background labels from 
    https://github.com/dilidili/react-drawing-board/pull/25 so they are 
    ready when needed.

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,8 +1,9 @@
 import enUS from './en-US';
 import zhCN from './zh-CN';
 import trTR from './tr-TR';
+import ptBR from './pt-BR';
 
-export type localeType = 'en-US' | 'zh-CN' | 'tr-TR' | 'en' | 'zh' | 'tr';
+export type localeType = 'en-US' | 'zh-CN' | 'tr-TR' | 'en' | 'zh' | 'tr' | 'pt-BR';
 
 export default {
   messages: {
@@ -12,5 +13,6 @@ export default {
     ['en']: enUS,
     ['zh']: zhCN,
     ['tr']: enUS,
+    ['pt-BR']: ptBR,
   },
 };
