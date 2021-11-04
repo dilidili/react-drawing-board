89ea35102b4fd3aa133e0d048586db2bbf38b7c2
Gabriel Guilhoto <gabriel.guilhoto@geekie.com.br>
2021-11-04 18:30:47 -0300

    Adds pt-BR translation
    This commit adds support for a Brazilian Portuguese locale, by creating 
    a locale file on src/locales.  I included translations for the 
    background labels from 
    https://github.com/dilidili/react-drawing-board/pull/25 so they are 
    ready when needed.

---
+++ src/locales/pt-BR.ts
@@ -0,0 +1,25 @@
+export default {
+  'umi.block.sketch.select': 'Selecionar',
+  'umi.block.sketch.pencil': 'Lápis (P)',
+  'umi.block.sketch.shape': 'Formas (R)',
+  'umi.block.sketch.text': 'Texto (T)',
+  'umi.block.sketch.image': 'Imagem',
+  'umi.block.sketch.background': 'Fundo',
+  'umi.block.sketch.undo': 'Desfazer',
+  'umi.block.sketch.redo': 'Refazer',
+  'umi.block.sketch.clear': 'Limpar',
+  'umi.block.sketch.eraser': 'Borracha',
+  'umi.block.sketch.save': 'Salvar',
+
+  'umi.block.sketch.text.placeholder': 'Escreva o texto aqui',
+  'umi.block.sketch.text.size.small': 'Pequeno',
+  'umi.block.sketch.text.size.default': 'Médio',
+  'umi.block.sketch.text.size.large': 'Grande',
+
+  'umi.block.sketch.bg.select': 'Selecionar',
+  'umi.block.sketch.bg.layout': 'Layout',
+  'umi.block.sketch.bg.fill': 'Esticar',
+  'umi.block.sketch.bg.contain': 'Aumentar proporcionalmente',
+  'umi.block.sketch.bg.not-resize': 'Preservar dimensões',
+  'umi.block.sketch.bg.remove': 'Remover',
+};
