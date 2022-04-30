4900b81dda5706500ea6e55e13a5b9e5cf00e417
XuMM_12 <owiatsq@sina.cn>
2022-04-30 12:24:38 +0800

    fix: background image occupy the whole canvas

--- src/ImageTool.tsx
+++ src/ImageTool.tsx
@@ -63,6 +63,8 @@ export const drawBackgroundImage = (
       position = pos;
       _cacheBackgroundPosition[item.imageData] = pos;
       drawImage(item, context, position, id, rerender);
+    }, {
+      imageSize: "contain"
     });
   }
 };
@@ -72,6 +74,9 @@ export const onImageComplete = (
   canvas: HTMLCanvasElement,
   viewMatrix: number[],
   handleCompleteOperation: (tool?: Tool, data?: Image, pos?: Position) => void,
+  options?: {
+    imageSize?: 'contain'
+  },
 ) => {
   const image = new Image();
 
@@ -82,19 +87,47 @@ export const onImageComplete = (
     const offsetWidth = canvas.offsetWidth;
     const offsetHeight = canvas.offsetHeight;
 
+    const imageRatio = imageWidth / imageHeight;
+    const canvasRatio = offsetWidth / offsetHeight;
+
+    let targetImageWidth = imageWidth;
+    let targetImageHeight = imageHeight;
+
+    const clientPos = {
+      clientX: 0,
+      clientY: 0,
+    }
+    const clientPosEnd = {
+      clientX: 0,
+      clientY: 0,
+    }
+    if (options?.imageSize == "contain") {
+      if (imageRatio >= canvasRatio) {
+        targetImageWidth = offsetWidth;
+        targetImageHeight = targetImageWidth / imageRatio;
+      } else {
+        targetImageHeight = offsetHeight;
+        targetImageWidth = targetImageHeight * imageRatio;
+      }
+
+      clientPos.clientX = left + (offsetWidth / 2 - targetImageWidth / 2);
+      clientPos.clientY = top + (offsetHeight / 2 - targetImageHeight / 2);
+      clientPosEnd.clientX = left + (offsetWidth / 2 + targetImageWidth / 2);
+      clientPosEnd.clientY = top + (offsetHeight / 2 + targetImageHeight / 2);
+    } else {
+      clientPos.clientX = left + (offsetWidth / 2 - targetImageWidth / 4);
+      clientPos.clientY = top + (offsetHeight / 2 - targetImageHeight / 4);
+      clientPosEnd.clientX = left + (offsetWidth / 2 + targetImageWidth / 4);
+      clientPosEnd.clientY = top + (offsetHeight / 2 + targetImageHeight / 4);
+    }
+
     const pos = mapClientToCanvas(
-      {
-        clientX: left + (offsetWidth / 2 - imageWidth / 4),
-        clientY: top + (offsetHeight / 2 - imageHeight / 4),
-      } as MouseEvent<HTMLCanvasElement>,
+      clientPos as MouseEvent<HTMLCanvasElement>,
       canvas,
       viewMatrix,
     );
     const posEnd = mapClientToCanvas(
-      {
-        clientX: left + (offsetWidth / 2 + imageWidth / 4),
-        clientY: top + (offsetHeight / 2 + imageHeight / 4),
-      } as MouseEvent<HTMLCanvasElement>,
+      clientPosEnd as MouseEvent<HTMLCanvasElement>,
       canvas,
       viewMatrix,
     );
