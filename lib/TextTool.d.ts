206d6ae2990bb60134e8a8b015130de4027b49b2
XuMM_12 <owiatsq@sina.cn>
2022-05-02 21:24:02 +0800

    feat: add build files

--- lib/TextTool.d.ts
+++ lib/TextTool.d.ts
@@ -14,4 +14,9 @@ export declare const onTextMouseDown: (e: {
 export declare const onTextComplete: (refInput: RefObject<HTMLDivElement>, refCanvas: RefObject<HTMLCanvasElement>, viewMatrix: number[], scale: number, handleCompleteOperation: (tool?: Tool, data?: Text, pos?: Position) => void, setCurrentTool: (tool: Tool) => void) => void;
 export declare const font = "\"PingFang SC\", -apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, \"Helvetica Neue\", Helvetica, \"Hiragino Sans GB\", \"Microsoft YaHei\", SimSun, sans-serif, \"localant\"";
 export declare const drawText: (item: Text, context: CanvasRenderingContext2D, pos: Position) => void;
-export declare const useTextDropdown: (currentToolOption: ToolOption, setCurrentToolOption: (option: ToolOption) => void, setCurrentTool: (tool: Tool) => void, intl: IntlShape, prefixCls: string) => JSX.Element;
+export declare const useTextDropdown: (config: {
+    currentToolOption: ToolOption;
+    setCurrentToolOption: (option: ToolOption) => void;
+    setCurrentTool: (tool: Tool) => void;
+    prefixCls: string;
+}) => JSX.Element;
